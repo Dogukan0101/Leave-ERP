35a0584ef3ca4ff7468b7e10d645289e02cc7718
Doğukan <86402638+Dogukan0101@users.noreply.github.com>
2024-07-01 18:18:52 +0300

    Initial commit
    The project is created and database connection is ensured.

---
+++ userERP/src/main/java/com/kafein/userERP/UserErpApplication.java
@@ -0,0 +1,13 @@
+package com.kafein.userERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserErpApplication.class, args);
+	}
+
+}
